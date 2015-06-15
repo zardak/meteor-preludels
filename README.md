99591e4c3048e88771d6c7ba4300e94c4dfdf695
zardak <zardakss@gmail.com>
2015-06-15 03:55:28 +0300

    0.1.0

---
+++ README.md
@@ -0,0 +1,6 @@
+# meteor-preludels
+[prelude.ls](http://www.preludels.com/) functionally oriented utility library packaged for meteor.
+
+It is powerful and flexible.
+Almost all of its functions are curried.
+It is written in, and is the recommended base library for, [LiveScript](http://livescript.net/).
---
+++ package.js
@@ -0,0 +1,13 @@
+Package.describe({
+  name: 'zardak:preludels',
+  version: '0.1.0',
+  summary: 'prelude.ls is a functionally oriented utility library. It is written in, and is the recommended base library for, LiveScript.',
+  git: 'https://github.com/zardak/meteor-preludels'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('0.9.3');
+  api.export('prelude');
+  api.addFiles('prelude-browser.js');
+  api.addFiles('require-preludels.js');
+});
